06ae991d9471e0e7a2441691d79f9f229fc86d5b
Pavyn <pavyns@gmail.com>
2024-08-28 21:20:21 +0200

    manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -23,8 +23,10 @@ client_scripts {
 }
 
 server_scripts {
+    '@oxmysql/lib/MySQL.lua',
     'PolyZone/server.lua',
     'lua/server/cacheFile.lua',
+    'lua/Server/cameraPerms.lua'
 }
 
 ui_page 'html/index.html'
@@ -32,3 +34,5 @@ ui_page 'html/index.html'
 files {
     'html/**.*',
 }
+
+dependencies { 'oxmysql' }
